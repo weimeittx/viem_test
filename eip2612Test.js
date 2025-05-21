b14546df62d268d043f1fe12ac9c46a0268a5939
weimeittx <422450455@qq.com>
2025-05-21 22:18:34 +0800

    --

--- eip2612Test.js
+++ eip2612Test.js
@@ -5,10 +5,9 @@ import eip2612 from './abi/eip2612.js'
 
 const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545')
 ;(async () => {
-    //发送的钱包
+    //发送的钱包, 需要签名
     let wallet = new Wallet(
         '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-        // '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
         provider
     )
 
